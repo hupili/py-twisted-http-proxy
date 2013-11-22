9682a50cfeed3bed404ed5e43a2da5b9e01e3e3d
HU Pili <hpl1989@gmail.com>
2013-11-22 16:34:49 +0800

    clean

--- proxy.py
+++ proxy.py
@@ -1,10 +1,5 @@
 from twisted.web import proxy, http
 from twisted.internet import reactor
-#from twisted.python import log
-import sys
-#log.startLogging(sys.stdout)
-
-import pickle
 import json
 
 logfile = 'log'
@@ -18,30 +13,17 @@ logging.basicConfig(
         )
 
 class LogProxy(proxy.Proxy):
-    #def allHeadersReceived(self):
-    #    #print "hhhh"
-    #    #super(LogProxy, self).allHeadersReceived()
-    #    proxy.Proxy.allHeadersReceived(self)
-    #    req = self.requests[-1]
-    #    #sys.stderr.write(str(req) + '\n')
-    #    sys.stderr.write(str(req.requestHeaders) + '\n')
-    #    sys.stderr.write(str(req.path) + '\n')
-    #    #pickle.dump(req, open('req.pickle', 'w'))
-
     def allContentReceived(self):
+        # Modify default method to log important information
+        # in a desired format
         proxy.Proxy.allContentReceived(self)
         req = self.requests[-1]
-        #sys.stderr.write('method:' + str(req.method) + '\n')
-        #sys.stderr.write('uri:' + str(req.uri) + '\n')
-        #sys.stderr.write('path:' + str(req.path) + '\n')
-        #sys.stderr.write('headers:' + str(req.requestHeaders) + '\n')
         info = {
             'method': req.method,
             'uri': req.uri,
             'path': req.path,
             'headers': req.getAllHeaders()
         }
-        #sys.stdout.write(json.dumps(info) + '\n')
         logging.info(json.dumps(info) + '\n')
 
 class ProxyFactory(http.HTTPFactory):
